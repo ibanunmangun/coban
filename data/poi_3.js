79f7bac279c6ed89bc339927b15aca85e2892da5
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-12-10 03:54:30 +0800

    Add files via upload

---
+++ data/poi_3.js
@@ -0,0 +1,21 @@
+var json_poi_3 = {
+"type": "FeatureCollection",
+"name": "poi_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Wisata Goa Jepang", "desc": "Point of Interest", "x": "112.5341439", "y": "-7.9164684" }, "geometry": { "type": "Point", "coordinates": [ 112.5341439, -7.9164684 ] } },
+{ "type": "Feature", "properties": { "name": "Batu Offroad", "desc": "Point of Interest", "x": "112.5343291", "y": "-7.8717167" }, "geometry": { "type": "Point", "coordinates": [ 112.5343291, -7.8717167 ] } },
+{ "type": "Feature", "properties": { "name": "Coban swing park Talun", "desc": "Point of Interest", "x": "112.5160551", "y": "-7.8043421" }, "geometry": { "type": "Point", "coordinates": [ 112.5160551, -7.8043421 ] } },
+{ "type": "Feature", "properties": { "name": "Pagupon Camp", "desc": "Point of Interest", "x": "112.5163345", "y": "-7.8001732" }, "geometry": { "type": "Point", "coordinates": [ 112.5163345, -7.8001732 ] } },
+{ "type": "Feature", "properties": { "name": "Warung Oyot", "desc": "Point of Interest", "x": "112.5163252", "y": "-7.8017444" }, "geometry": { "type": "Point", "coordinates": [ 112.5163252, -7.8017444 ] } },
+{ "type": "Feature", "properties": { "name": "Wisata Kebun Bunga Coban Talun", "desc": "Point of Interest", "x": "112.5200093", "y": "-7.80225" }, "geometry": { "type": "Point", "coordinates": [ 112.5200093, -7.80225 ] } },
+{ "type": "Feature", "properties": { "name": "Coban Talun Waterfall", "desc": "Point of Interest", "x": "112.5164789", "y": "-7.8039184" }, "geometry": { "type": "Point", "coordinates": [ 112.5164789, -7.8039184 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Punten", "desc": "Public Facilities", "x": "112.5305337", "y": "-7.838977957" }, "geometry": { "type": "Point", "coordinates": [ 112.5305337, -7.838977957 ] } },
+{ "type": "Feature", "properties": { "name": "Pemadam Kebakaran Batu", "desc": "Public Facilities", "x": "112.525378", "y": "-7.870299213" }, "geometry": { "type": "Point", "coordinates": [ 112.525378, -7.870299213 ] } },
+{ "type": "Feature", "properties": { "name": "Polres Batu", "desc": "Public Facilities", "x": "112.5541464", "y": "-7.905514551" }, "geometry": { "type": "Point", "coordinates": [ 112.5541464, -7.905514551 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pertamina 54.651.67", "desc": "Public Facilities", "x": "112.5423481", "y": "-7.86467713" }, "geometry": { "type": "Point", "coordinates": [ 112.5423481, -7.86467713 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Al-Fattah", "desc": "Public Facilities", "x": "112.5681194", "y": "-7.891652739" }, "geometry": { "type": "Point", "coordinates": [ 112.5681194, -7.891652739 ] } },
+{ "type": "Feature", "properties": { "name": "Area Parkir Coban Talun", "desc": "Public Facilities", "x": "112.5238096", "y": "-7.908120764" }, "geometry": { "type": "Point", "coordinates": [ 112.5238096, -7.908120764 ] } },
+{ "type": "Feature", "properties": { "name": "Puskesmas Pembantu GUnungsari Or Batu (City)", "desc": "Public Facilities", "x": "112.5141607", "y": "-7.840428252" }, "geometry": { "type": "Point", "coordinates": [ 112.5141607, -7.840428252 ] } }
+]
+}
